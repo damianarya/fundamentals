c0964c05179136f1749f093d968c142ec33dd788
Damian Arya <dahakcyrus@yahoo.com>
2019-09-27 20:04:33 -0600

    fixed fundamentals

---
+++ objective oriented & APIs.js
@@ -0,0 +1,25 @@
+/*
+*objects, Interfaces, and APIs
+ */
+
+let dog = {
+	name: "doggo",
+	color: "brown",
+	breed: "pug",
+	size: "small",
+	bark: function (typeOfBark){
+		console.log ("bark!");
+	},
+};
+
+function x(y) {
+	y.num = y.num + 5;
+	consloe.log(y);
+}
+
+let y = {
+	name: "tom",
+	num: 10,
+};
+x(y);
+console.log(y);
